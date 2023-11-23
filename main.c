09877fd3ec278c215b55868913da8517bb2e1730
Serl32 <s8532767@gmail.com>
2023-11-23 23:13:06 +0100

    Edit main c file

--- main.c
+++ main.c
@@ -5,27 +5,19 @@
 
 int main(int argc, char *argv[]) {
 
-    //void greeting() {
-    //    time_t t;
-    //    time(&t);
-
-    //    printf("\n# \tRe-ToDo Version %s %100s\n", CURRENT_VERSION, ctime(&t));
-
-    //    printf("# A Simple To-Do List Program written in C by Serl.\n");
-    //    printf("# The data files are located in the same folder where the program is located.\n");
-    //    printf("# for easier access and backup, this program will save the files in the .txt format or JSON format.\n");
-    //    printf("# A seperate database and management features aren't currently available.\n");
-    //    printf("# Use \"-h\" as the artument for more information and help.\n");
-    //    printf("# The main.c source file was corrupted and rewriting the code again.\n");
-    //}
+    // If Statements for performing specific functions
+    // -h, -l, -n, -q, -c, init
 
     if (argc != 1) {
         if (strcmp(argv[1], "-h")==0) {
-            const int sz = 255;
 
+            const int sz = 255;
             char str[sz];
+
             FILE *fp;
+
             fp = fopen("help.txt", "r");
+            
             if (fp == NULL) {
                 printf("Text file isn't available (Create one with \"help.txt\").");
             } else {
@@ -37,38 +29,59 @@ int main(int argc, char *argv[]) {
 
             fclose(fp);
         } else if (strcmp(argv[1], "-l")==0) {
+            // List all ToDos this is supposed to List the logs.
             LIST_ALL(argv[2]);
+
         } else if (strcmp(argv[1], "-n")==0) {
+
+            //Create New ToDo / SubToDo
             if (argc == 3) {
                 CREATE_NEW(argv[2]);
             } else if (argc == 4) {
                 CREATE_SUBTODO(argv[2], argv[3]);
             } else {
-                printf("Not enough arguments, Add your title as \"Your title\" as argument.\n");
+                printf("Not enough arguments, Add title \"Your title\" as argument with \"-n\"\n");
             }
+
         } else if (strcmp(argv[1], "-q")==0) {
+
+            // Query One ToDo, this is supposed to list all the unfinished ToDos.
             if (argc == 3) {
                 QUERY_ONE(argv[2]);
             } else {
                 printf("Not enough arguments. Add your database name with your arguments.\n");
             }
+
         } else if (strcmp(argv[1], "-c")==0) {
+
+            // Set one ToDo Completed
             if (argc == 3) {
                 COMPLETE_ONE(argv[2]);
             } else {
-                printf("Not enough arguments. Add your ToDo Id as an arguments.\n");
+                printf("Not enough arguments. Add your ToDo Id as arguments.\n");
             }
+
         } else if (strcmp(argv[1], "init")==0) {
+
+            // Create New Database
             if (argc == 3) {
                 CREATE_DB(argv[2]);
             } else {
                 printf("Not enough arguments. Add your database name with your current argument.\n");
             }
+
         } else {
+
+            // Output for the wrong/undefined argument/arguments
             printf("Argument %s is undefined. Try \"-h\" for more information.\n");
         }
+
     } else {
+
+        // Funtion for no argument
         greeting();
+
     }
+
     return 0;
 }
