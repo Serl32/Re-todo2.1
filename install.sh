98d5145a4b838dfe422558234cd10e82cd451727
Serl32 <s8532767@gmail.com>
2023-11-23 22:24:37 +0100

    Edit bash script

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 echo "Welcome to installation wizard for installing Re-ToDo2.1 Test Version 2.1.1"
 
+read -p "Do you want to continue the installation? (y/n):" confirm && [[ $confirm ==[yY] || $confirm == [yY][eE][sS] ]] || exit 1;
+
 # Setup Location
 LOCATION="$(pwd)/data"
 DATEANDTIME="Date and Time $(date '+%H:%M:%S %d/%m/%Y')";
@@ -13,15 +15,13 @@ if [ -d "$LOCATION" ]; then
     echo "\"data/\" Directory exist."
     echo "Checking for the subsequent files."
 
-    if [[ -f "$LOCATION/logs.txt" && -f "$LOCATION/list.txt" && -f "$LOCATION/todo.txt" && -f "$LOCATION/done.txt" ]]; then
+    if [[ -f "$LOCATION/logs.txt" && -f "$LOCATION/list.txt" ]]; then
         echo "All subsequent files exist."
     else
         echo "Data files Doesn't exist. Creating New Files."
 
         echo "Re-ToDo2.1 Test Version 2.1.1 - $DATEANDTIME - LOGS FIlES" >> $LOCATION/logs.txt;
         echo "Re-ToDo2.1 Test Version 2.1.1 - $DATEANDTIME - LIST" >> $LOCATION/list.txt;
-        echo "Re-ToDo2.1 Test Version 2.1.1 - $DATEANDTIME - TODO LIST" >> $LOCATION/todo.txt;
-        echo "Re-ToDo2.1 Test Version 2.1.1 - $DATEANDTIME - COMPLETED" >> $LOCATION/done.txt;
 
         echo "Subsequent Files Created."
     fi
@@ -33,18 +33,17 @@ else
     
     echo "Re-ToDo2.1 Test Version 2.1.1 - $DATEANDTIME - LOGS FIlES" >> $LOCATION/logs.txt;
     echo "Re-ToDo2.1 Test Version 2.1.1 - $DATEANDTIME - LIST" >> $LOCATION/list.txt;
-    echo "Re-ToDo2.1 Test Version 2.1.1 - $DATEANDTIME - TODO LIST" >> $LOCATION/todo.txt;
-    echo "Re-ToDo2.1 Test Version 2.1.1 - $DATEANDTIME - COMPLETED" >> $LOCATION/done.txt;
 
     echo "All files created."
 fi
 
 # Compile c code with gcc
-gcc -o test main.c corelib.h -lsqlite3;
-echo "Installed and compiled C code."
-echo "Use ./test init [DATABASE NAME] to initialize and get started with the program";
+gcc -o re main.c corelib.h -lsqlite3;
+echo "Compile C Code - \"gcc -o re main.c corelib.h -lsqlite3\"";
+echo "Setup Complete.";
+echo "Use ./re init [DATABASE NAME] to initialize the database.";
+echo "use \"-h\" for more information.";
 
-echo "Setup Complete."
 echo "[$DATEANDTIME]: INITIAL SETUP COMPLETE. IN [$LOCATION]." >> $LOCATION/logs.txt;
 
 exit 0;
