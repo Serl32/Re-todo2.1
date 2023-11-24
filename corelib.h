c6ef815ff0eb7b6cbe418855cab641a3152c466a
Serl32 <s8532767@gmail.com>
2023-11-24 12:18:11 +0100

    Edit functions in functions library

--- corelib.h
+++ corelib.h
@@ -192,8 +192,47 @@ int CREATE_SUBTODO(char td_id[], char td_title[]) {
     return 0;
 }
 
-void COMPLETE_ONE(char td_id[]) {
-    
+void COMPLETE_ONE(int td_id) {
+    char recent[] = get_recent();
+
+    sqlite3 *db;
+    char *err_msg = 0;
+    sqlite3_stmt *res;
+
+    int rc = sqlite3_open(recent, &db);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+
+        return 1;
+    }
+
+    char *sql = "UPDATE Todos SET Status = 1, Date_Completed = datetime('now') WHERE Id = @id";
+
+    rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);
+
+    if (rc == SQLITE_OK) {
+        int idx = sqlite3_bind_parameter_index(res, "@id");
+        int value = td_id;
+        sqlite3_bind_int(res, idx, value);
+    } else {
+        fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
+    }
+
+    int step = sqlite3_step(res);
+
+    if (step == SQLITE_ROW) {
+
+        printf("%s: ", sqlite3_column_text(res, 0));
+        printf("%s\n", sqlite3_column_text(res, 1));
+
+    }
+
+    sqlite3_finalize(res);
+    sqlite3_close(db);
+
+    return 0;
 }
 
 int callback(void *, int, char **, char **);
